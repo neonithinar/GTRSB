0a157d84dedee9ab41df76a0ae1f5f0bdfd7b735
neonithinar <neonithinar@gmail.com>
2021-03-27 10:22:07 +0530

    Downloaded dataset and added .gitignore some csv files did got added. but thats ok.

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -6,14 +6,24 @@ current_dir = os.getcwd()
 dataset_dir = current_dir + '/datasets'
 dataset_dir_filelist = os.listdir(dataset_dir)
 GTRSB_url = 'https://sid.erda.dk/public/archives/daaeac0d7ce1152aea9b61d9f1e19370/GTSRB_Final_Training_Images.zip'
+download_file = current_dir+'/GTSRB_Final_Training_Images.zip'
 
 if 'GTRSB' not in dataset_dir_filelist:
-    print("GTRSB folder not found ")
-    print('Attempting to download GTSRB_Final_Training_Images.zip from \n', GTRSB_url)
-    r = requests.get(GTRSB_url, allow_redirects=True)
-    open('GTSRB_Final_Training_Images.zip', 'wb').write(r.content)
-    print('Download complete')
-    download_file = dataset_dir+'/GTSRB_Final_Training_Images.zip'
-    with ZipFile(download_file, 'r') as zip_ref:
-        extractall(dataset_dir)
-    os.remove(download_file)
+    if os.path.exists(download_file):
+
+        with ZipFile(download_file, 'r') as zip_ref:
+            zip_ref.extractall(path = dataset_dir)
+            zip_ref.close()
+        os.remove(download_file)
+
+    else:
+        print("GTRSB folder not found ")
+        print('Attempting to download GTSRB_Final_Training_Images.zip from \n', GTRSB_url)
+        r = requests.get(GTRSB_url, allow_redirects=True)
+        open('GTSRB_Final_Training_Images.zip', 'wb').write(r.content)
+        print('Download complete')
+
+        with ZipFile(download_file, 'r') as zip_ref:
+            extractall(dataset_dir)
+        os.remove(download_file)
+
